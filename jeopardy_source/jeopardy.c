f71c00bd13263fc76d33a9d30c2c4108724ab112
Kevaunjh <kevaunjh@gmail.com>
2024-02-06 17:07:37 -0500

    kev push 1

--- jeopardy_source/jeopardy.c
+++ jeopardy_source/jeopardy.c
@@ -36,14 +36,39 @@ int main(int argc, char *argv[])
 
     // Display the game introduction and initialize the questions
     initialize_game();
-
+	int count = 0;
     // Prompt for players names
-    
+	while (count != NUM_PLAYERS) {
+		if (count == 0 ) {
+			char str[];
+		printf("What is the current player name: %d", i);
+		scanf ("%16s", str);
+		strcpy(players[i], str);
+		}
+		else if (count == 1) {
+			char str2[];
+			printf("What is the current player name: %d", i);
+			scanf (players[i], str2);
+		}
+		else if (count == 2){
+			char str3[];
+			printf("What is the current player name: %d", i);
+			scanf(players[i], str3);
+		}
+		else if (count == 3){
+			char str4[];
+			printf("What is the current player name: %d", i);
+			scanf(players[i], str4);
+		}
+		else{
+		}
+
+		count++;
+	}	
     // initialize each of the players in the array
 
     // Perform an infinite loop getting command input from users until game ends
-    while (fgets(buffer, BUFFER_LEN, stdin) != NULL)
-    {
+    while (fgets(buffer, BUFFER_LEN, stdin) != NULL) {
         // Call functions from the questions and players source files
 
         // Execute the game until all questions are answered
--- jeopardy_source/players.c
+++ jeopardy_source/players.c
@@ -12,8 +12,13 @@
 
 // Returns true if the player name matches one of the existing players
 bool player_exists(player *players, int num_players, char *name)
-{
-    return false;
+{	
+
+	if () {
+		
+	} else {
+    	return false;
+	}
 }
 
 // Go through the list of players and update the score for the 
